57b7182ee86493e4372b568a2834d197d5c44b95
VierowSystem <129748358+VierowSystem@users.noreply.github.com>
2023-08-21 11:34:00 +0530

    Update settings.py

--- OfficePro/settings.py
+++ OfficePro/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@=5^)9-j_u$$nvz_%t+e_gk#&q#%m6g&zk$ncls1i^+y^ki34!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
